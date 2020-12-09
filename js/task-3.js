57ddf493acae2668b89a34effa1e919b1082d6c5
Yulia Sukhar <yulia-s@i.ua>
2020-12-09 12:18:09 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,9 @@ const findLongestWord = function (string) {
   let words = string.split(' ');
   let longestWord = words[0];
   for (let word of words) {
-    if (word.length > longestWord.length) longestWord = word;
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
   }
 
   return longestWord;
